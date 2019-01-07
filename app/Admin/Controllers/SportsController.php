921ab78391933ae0a8c4aca6727a3465524952e3
kakashen <1519176958@qq.com>
2019-01-07 14:05:19 +0800

    sports模块 首页

--- app/Admin/Controllers/SportsController.php
+++ app/Admin/Controllers/SportsController.php
@@ -4,19 +4,50 @@ namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Sports;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
 use Illuminate\Http\Request;
 
 class SportsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Index interface.
      *
-     * @return \Illuminate\Http\Response
+     * @return Content
      */
     public function index()
     {
-        //
+      return Admin::content(function (Content $content) {
+
+        $content->header('header');
+        $content->description('description');
+
+        $content->body($this->grid());
+      });
     }
+
+  /**
+   * Make a grid builder.
+   *
+   * @return Grid
+   */
+  protected function grid()
+  {
+    return Admin::grid(Sports::class, function (Grid $grid) {
+
+      $grid->id('ID')->sortable();
+      $grid->name('推荐人');
+      $grid->recommended_at('推荐日期');
+      $grid->host('排序');
+      $grid->guest();
+      $grid->first();
+      $grid->last();
+      $grid->recommend();
+      $grid->chain();
+      $grid->remark();
+    });
+  }
 
     /**
      * Show the form for creating a new resource.
