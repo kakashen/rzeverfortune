393e5c6f6f11bf69fc4ce80102855fc5c46803c0
kaka <1519176958@qq.com>
2019-01-08 17:10:45 +0800

    添加 推荐人模块 控制器/model

---
+++ app/Admin/Controllers/RecommenderController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Recommender;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class RecommenderController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('推荐人');
+            $content->description('各位大神');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(Recommender::class, function (Grid $grid) {
+
+            $grid->id('ID')->sortable();
+            $grid->name('姓名');
+            $grid->remark('备注');
+
+            //$grid->created_at();
+            //$grid->updated_at();
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(Recommender::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+            $form->text('name', '姓名');
+            $form->text('remark', '备注')->default('无');
+
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
